7856d4a4f1cc83a16d1fb9db1364ca8933dd6965
magl88 <magl88net@gmail.com>
2023-08-30 18:28:12 +0200

    add footer

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer id="footer" class="footer">
+  <div class="container">
+    <div class="footer__copy">
+      <?php esc_html_e("Copyright &copy; 1988-". date("Y") . " " . get_bloginfo('name'), 'm88them'); ?>
+    </div>
+  </div>
+</footer>
