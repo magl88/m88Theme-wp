da29f4312fc55538dda1d9e88da662e89a27d427
magl88 <magl88net@gmail.com>
2023-08-30 18:29:40 +0200

    add head block

---
+++ inc/blocks/head.php
@@ -0,0 +1,23 @@
+<?php include ('picture.php');?>
+
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <!--  ==========================-->
+  <?php include 'favicon.php'; ?>
+  <!--  ==========================-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+    rel="stylesheet">
+  <!--  ==========================-->
+  <?php wp_head(); ?>
+  <!--  ==========================-->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
