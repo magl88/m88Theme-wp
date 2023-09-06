15ace2e55333c98a7ab48de55ae65580001a485f
magl88 <magl88net@gmail.com>
2023-09-06 10:36:58 +0200

    Update demo index page

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
-<?php include 'inc/blocks/head.php'; ?>
+<html lang="<?php echo get_bloginfo('language');?>">
+
+<head>
+  <?php wp_head(); ?>
+</head>
 
 <body <?php body_class() ?>>
   <?php wp_body_open(); ?>
   <div class="wrapper">
     <?php get_header(); ?>
     <main>
-      <?php get_template_part('sections/content'); ?>
-      <div class="test">
-        <a href="#footer">FOO</a>
-      </div>
+      <?php get_template_part('inc/sections/content'); ?>
     </main>
+
     <?php get_footer(); ?>
   </div>
   <?php wp_footer(); ?>
+
 </body>
 
 </html>
